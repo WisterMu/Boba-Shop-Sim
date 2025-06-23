eab4d8433ff1ae9b1975454d4aa0c72e7f577e3d
WisterMu <Shawn.wu5813@gmail.com>
2025-06-23 19:38:33 -0400

    Got player looking around

---
+++ BobaShopSim/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //variables
+    public float WalkSpeed = 5f;
+    public float SprintMultiplier = 2;
+    public float MouseSensitivityX = 1f;
+    public float MouseSensitivityY = 1f;
+    public Transform mainCamera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BobaShopSim/Assets/Player/playerLook.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class playerLook : MonoBehaviour
+{
+    //variables
+    public float mouseSensitivity = 100f;
+    [SerializeField] Transform playerBody;
+    float xRotation = 0f; //Makes sure camera doesn't spin without player
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //Locks cursor into  seperate frame
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+       
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);        
+    }
+}
