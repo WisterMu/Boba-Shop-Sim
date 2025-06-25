f7b1bc834b2a13f3725e30cf4bc39edb09ea7078
WisterMu <Shawn.wu5813@gmail.com>
2025-06-25 16:22:01 -0400

    still haven't figured out perfectly but character moves

--- BobaShopSim/Assets/Player/PlayerMovement.cs
+++ BobaShopSim/Assets/Player/PlayerMovement.cs
@@ -1,25 +1,33 @@
+using System.Numerics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     //variables
+    Rigidbody rb;
     public float WalkSpeed = 5f;
     public float SprintMultiplier = 2;
-    public float MouseSensitivityX = 1f;
-    public float MouseSensitivityY = 1f;
     public Transform mainCamera;
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        UnityEngine.Vector3 movement = new UnityEngine.Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rb.MovePosition(rb.position + movement * WalkSpeed * Time.fixedDeltaTime);
 
+        movement = Camera.main.transform.TransformDirection(movement);
+        movement = UnityEngine.Vector3.ProjectOnPlane(movement, UnityEngine.Vector3.up);
+
+        UnityEngine.Vector3 camDir = mainCamera.transform.forward;
+        camDir = UnityEngine.Vector3.ProjectOnPlane(camDir, UnityEngine.Vector3.up);
+
+        transform.forward = camDir;
     }
 }
