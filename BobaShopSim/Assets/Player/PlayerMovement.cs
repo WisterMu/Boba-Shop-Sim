33abb6bf661f67fbb13c79f3c745c7662a42ee8a
WisterMu <Shawn.wu5813@gmail.com>
2025-06-29 17:36:51 -0400

    player movement fixed

--- BobaShopSim/Assets/Player/PlayerMovement.cs
+++ BobaShopSim/Assets/Player/PlayerMovement.cs
@@ -29,11 +29,15 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        
+
         float movementAmount = Mathf.Abs(horizontal) + Mathf.Abs(vertical); //creates variable for combined variables
 
         var movementInput = new UnityEngine.Vector3(horizontal, 0, vertical).normalized; //normalizes movement
 
         var MovementDirection = mainCamera.transform.rotation * movementInput; //sets camera direction to be new forward
+
+        MovementDirection.y = 0;
 
         if (movementAmount > 0)
         {
