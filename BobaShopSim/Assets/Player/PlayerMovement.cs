e65d1e90b32a835bd580eb936f028d46c1dbbd7d
WisterMu <Shawn.wu5813@gmail.com>
2025-07-04 00:08:04 -0400

    so I can say I did something today
    even though it's really just making sure there are comments in my code...

--- BobaShopSim/Assets/Player/PlayerMovement.cs
+++ BobaShopSim/Assets/Player/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
--- BobaShopSim/Assets/Player/playerLook.cs
+++ BobaShopSim/Assets/Player/playerLook.cs
@@ -18,14 +18,14 @@ public class playerLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; //get horizontal rotation
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; //get vertical rotation
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //clamps limit of vertical rotation to between -90 and 90 degrees
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //puts rotation into effect
+        playerBody.Rotate(Vector3.up * mouseX); //makes player treat the camera direction as new forward direction
     }
 
     
